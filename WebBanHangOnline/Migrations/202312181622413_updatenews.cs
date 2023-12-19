aa0de55ba5e22d3517e2f517257c5660b97dab8d
phancongdanh1542k2@gmail.com <phancongdanh1542k2@gmail.com>
2023-12-19 21:04:34 +0700

    Project v1.0

---
+++ WebBanHangOnline/Migrations/202312181622413_updatenews.cs
@@ -0,0 +1,26 @@
+﻿namespace WebBanHangOnline.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updatenews : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.tb_News", "CategoryId", "dbo.tb_Category");
+            DropIndex("dbo.tb_News", new[] { "CategoryId" });
+            AlterColumn("dbo.tb_News", "CategoryId", c => c.Int());
+            CreateIndex("dbo.tb_News", "CategoryId");
+            AddForeignKey("dbo.tb_News", "CategoryId", "dbo.tb_Category", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.tb_News", "CategoryId", "dbo.tb_Category");
+            DropIndex("dbo.tb_News", new[] { "CategoryId" });
+            AlterColumn("dbo.tb_News", "CategoryId", c => c.Int(nullable: false));
+            CreateIndex("dbo.tb_News", "CategoryId");
+            AddForeignKey("dbo.tb_News", "CategoryId", "dbo.tb_Category", "Id", cascadeDelete: true);
+        }
+    }
+}
---
+++ WebBanHangOnline/Migrations/202312181624265_updateposts.cs
@@ -0,0 +1,26 @@
+﻿namespace WebBanHangOnline.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updateposts : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.tb_Posts", "CategoryId", "dbo.tb_Category");
+            DropIndex("dbo.tb_Posts", new[] { "CategoryId" });
+            AlterColumn("dbo.tb_Posts", "CategoryId", c => c.Int());
+            CreateIndex("dbo.tb_Posts", "CategoryId");
+            AddForeignKey("dbo.tb_Posts", "CategoryId", "dbo.tb_Category", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.tb_Posts", "CategoryId", "dbo.tb_Category");
+            DropIndex("dbo.tb_Posts", new[] { "CategoryId" });
+            AlterColumn("dbo.tb_Posts", "CategoryId", c => c.Int(nullable: false));
+            CreateIndex("dbo.tb_Posts", "CategoryId");
+            AddForeignKey("dbo.tb_Posts", "CategoryId", "dbo.tb_Category", "Id", cascadeDelete: true);
+        }
+    }
+}
